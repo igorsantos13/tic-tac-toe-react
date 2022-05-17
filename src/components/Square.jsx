49922735e3a0cb6d56274bbdc4ed72a7e50fecf3
Igor Santos <igorsantos9513@outlook.com>
2022-05-17 13:41:57 -0300

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../App.css'
+
+export default function Square({val, chooseSquare}) {
+  return (
+    <div className='square' onClick={chooseSquare}>{val}</div>
+  )
+}
